e9380eb22b10166ddb0208447c5a75854d3050ac
Dien Chu <dienchuit@gmail.com>
2024-06-02 10:54:46 +0700

    Create route resource

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,34 @@
 <?php
 
 use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\User\UserController;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Buyer
+Route::resource('buyers', BuyerController::class)
+    ->only(['index', 'show']);
 
-Route::resource('buyers', BuyerController::class);
+// Categories
+Route::resource('categories', CategoryController::class)
+    ->except(['create', 'edit']);
+
+// Products
+Route::resource('products', ProductController::class)
+    ->only(['index', 'show']);
+
+// Sellers
+Route::resource('sellers', SellerController::class)
+    ->only(['index', 'show']);
+
+// Transactions
+Route::resource('transactions', Transaction::class)
+    ->only(['index', 'show']);
+
+// Category
+Route::resource('users', UserController::class)
+    ->except(['create', 'edit']);
