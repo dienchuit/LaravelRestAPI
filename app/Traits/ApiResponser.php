5da38e44f66ef04c5bff36ce8082fc31f2bc929b
Dien Chu <dienchuit@gmail.com>
2024-06-07 14:01:09 +0700

    Cache

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 
 trait ApiResponser
 {
@@ -21,14 +23,18 @@ trait ApiResponser
     {
 
         if ($collection->isEmpty()) {
-			return $this->successResponse(['data' => $collection], $code);
-		}        
+            return $this->successResponse(['data' => $collection], $code);
+        }
 
-		$transformer = $collection->first()->transformer;
+        $transformer = $collection->first()->transformer;
+
+        $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
+
         $collection = $this->transformData($collection, $transformer);
-        // $collection = $this->filterData($collection);
-        
+        $collection = $this->cacheResponse($collection);
+
         return $this->successResponse($collection, $code);
     }
 
@@ -36,35 +42,64 @@ trait ApiResponser
     {
         $transformer = $instance->transformer;
         $instance = $this->transformData($instance,  $transformer);
+        $instance = $this->cacheResponse($instance);
 
         return $this->successResponse($instance, $code);
     }
 
     protected function sortData(Collection $collection, $transformer)
     {
-        if(request()->has('sort_by')){
+        if (request()->has('sort_by')) {
             $attribute = $transformer::originalAttribute(request()->sort_by);
             $collection = $collection->sortBy($attribute);
         }
         return $collection;
     }
 
-    protected function filterData(Collection $collection)
-	{
-		foreach (request()->query() as $attribute => $value) {
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $attribute => $value) {
+            $attribute = $transformer::originalAttribute($attribute);
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
 
-			if (isset($attribute, $value)) {
-				$collection = $collection->where($attribute, $value);
-			}
-		}
-
-		return $collection;
-	}
+        return $collection;
+    }
 
     protected function transformData($data, $transformer)
-	{
-		$transformation = fractal($data, new $transformer);
+    {
+        $transformation = fractal($data, new $transformer);
 
-		return $transformation->toArray();
-	}
+        return $transformation->toArray();
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPgae = 10;
+        $results = $collection->slice(($page - 1) * $perPgae, $perPgae);
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPgae, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+        $paginated->withQueryString();
+
+        return $paginated;
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+        ksort($queryParams);
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 60, function () use ($data) {
+            return $data;
+        });
+    }
 }
